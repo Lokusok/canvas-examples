52610c586b49fe00398bb5e06ad6c24d4621c85e
Lokusok <lokusokdrok@gmail.com>
2024-01-07 15:41:13 +0300

    add webpack

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-const canvas = document.querySelector('#game');
+import './style.css';
+
+const canvas = document.createElement('canvas');
+canvas.id = 'game';
+canvas.width = 700;
+canvas.height = 300;
+
+document.body.append(canvas);
+
 const ctx = canvas.getContext('2d');
 const config = {
   square: {
-    width: 30,
-    height: 30,
+    width: 35,
+    height: 35,
   },
+  speeds: [1, 2, 3],
 };
-let coords = {x: 30, y: 10};
-let dx = 2;
-let dy = 2;
+let coords = {x: getRandomNumber(10, canvas.width), y: getRandomNumber(10, canvas.height)};
+let dx = getRandomElem(config.speeds);
+let dy = getRandomElem(config.speeds);
+
+function getRandomElem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
@@ -19,13 +36,12 @@ function gameLoop() {
   ctx.fillStyle = 'red';
   ctx.fillRect(coords.x, coords.y, config.square.width, config.square.height);
 
-  console.log(coords);
-  if (coords.x + config.square.width >= canvas.width || coords.x <= 0) {
-    dx *= -1;
+  if (coords.x + dx + config.square.width >= canvas.width || coords.x <= 0) {
+    dx = getRandomElem(config.speeds) * -Math.sign(dx);
   }
 
-  if (coords.y + config.square.height >= canvas.height || coords.y <= 0) {
-    dy *= -1;
+  if (coords.y + dy + config.square.height >= canvas.height || coords.y <= 0) {
+    dy = getRandomElem(config.speeds) * -Math.sign(dy);
   }
 
   requestAnimationFrame(gameLoop);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin  from 'mini-css-extract-plugin';
+
+const config = {
+  entry: './src/main.js',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'DVD example',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  }
+};
+
+export default config;
